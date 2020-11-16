cff10b5c2a88532981fd65705184ea772159d71d
alimentese <48683037+alimentese@users.noreply.github.com>
2020-11-16 15:42:45 +0300

    Scripts related to item and inventory were created

---
+++ SE320/Assets/Scripts/Inventory.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    private List<Item> itemList;
+    private Inventory inventory;
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject slot1;
+    [SerializeField] private GameObject slot2;
+    [SerializeField] private GameObject slot3;
+    [SerializeField] private GameObject slot4;
+    [SerializeField] private GameObject slot5;
+    [SerializeField] private GameObject slot6;
+    [SerializeField] private GameObject slot7;
+    [SerializeField] private GameObject slot8;
+    [SerializeField] private GameObject slot9;
+    [SerializeField] private GameObject slot10;
+    [SerializeField] private GameObject slot11;
+    [SerializeField] private GameObject slot12;
+
+    [SerializeField] private GameObject itemPrefabb;
+    private GameObject newItem;
+
+    private Item itemscript;
+
+    public Inventory() {
+        itemList = new List<Item>();
+        /*Debug.Log("Inventory");
+        AddItem(new Item {
+            itemType = Item.ItemType.HealthPotion,
+            amount = 5
+        });
+        AddItem(new Item {
+            itemType = Item.ItemType.HealthPotion,
+            amount = 5
+        });
+        Debug.Log(itemList.Count);*/
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public void AddItem(Item item) {
+        itemList.Add(item);
+    }
+
+    public void RemoveItem(string name) {
+        for (int i = 0; i < itemList.Count; i++) {
+            if(itemList[i].itemName == name) {
+                itemList.RemoveAt(i);
+                
+            }
+        }
+    }
+
+    public void CountItems() {
+        Debug.Log("toplam item: " + itemList.Count);
+    }
+
+    public List<Item> GetItemList() {
+        return itemList;
+    }
+
+    public void printList() {
+        for(int i = 0; i < itemList.Count; i++) {
+            Debug.Log(itemList[i]);
+        }
+    }
+
+
+    public void searchItem(string name) {
+        for (int i = 0; i < itemList.Count; i++) {
+            if(itemList[i].itemName == name) {
+                Debug.Log("Item found at " + i + ".slot.");
+            }
+        }
+
+    }
+
+
+
+    public void RefreshInventory() {
+        Debug.Log("inventory calisiyor");
+        inventory.CountItems();
+        foreach (Item itemInventory in inventory.GetItemList()) {
+            if (slot1.transform.childCount == 0) {
+                Vector3 newPosition = new Vector3(47, -47, 0);
+                newItem = Instantiate(itemPrefabb);
+                newItem.GetComponent<Item>().itemType = itemInventory.itemType;
+                newItem.GetComponent<Item>().itemAmountt = itemInventory.itemAmountt;
+                newItem.GetComponent<Item>().itemName = itemInventory.itemName;
+
+
+                newItem.transform.parent = slot1.transform;
+                newItem.transform.localPosition = newPosition;
+                Debug.Log("slot 1");
+            }
+            else if (slot2.transform.childCount == 0) {
+                Vector3 newPosition = new Vector3(47, -47, 0);
+                newItem = Instantiate(itemPrefabb);
+                newItem.GetComponent<Item>().itemType = itemInventory.itemType;
+                newItem.GetComponent<Item>().itemAmountt = itemInventory.itemAmountt;
+                newItem.GetComponent<Item>().itemName = itemInventory.itemName;
+
+
+
+                newItem.transform.parent = slot2.transform;
+                newItem.transform.localPosition = newPosition;
+                Debug.Log("slot 2");
+            }
+            else if (slot3.transform.childCount == 0) {
+                Vector3 newPosition = new Vector3(47, -47, 0);
+                newItem = Instantiate(itemPrefabb);
+                newItem.GetComponent<Item>().itemType = itemInventory.itemType;
+                newItem.GetComponent<Item>().itemAmountt = itemInventory.itemAmountt;
+                newItem.GetComponent<Item>().itemName = itemInventory.itemName;
+
+
+
+                newItem.transform.parent = slot3.transform;
+                newItem.transform.localPosition = newPosition;
+                Debug.Log("slot 3");
+            }
+            else if (slot4.transform.childCount == 0) {
+                Vector3 newPosition = new Vector3(47, -47, 0);
+                newItem = Instantiate(itemPrefabb);
+                newItem.GetComponent<Item>().itemType = itemInventory.itemType;
+                newItem.GetComponent<Item>().itemAmountt = itemInventory.itemAmountt;
+                newItem.GetComponent<Item>().itemName = itemInventory.itemName;
+
+
+
+                newItem.transform.parent = slot4.transform;
+                newItem.transform.localPosition = newPosition;
+                Debug.Log("slot 4");
+            }
+            else {
+                Debug.Log("inventory full");
+            }
+
+        }
+    }
+
+    // Start is called before the first frame update
+    public void Start() {
+        itemscript = itemPrefabb.GetComponent<Item>();
+        inventory.AddItem(new Item {
+            itemType = Item.ItemType.sword,
+            itemAmountt = 5,
+            itemName = "sword",
+
+        });
+
+        inventory.AddItem(new Item {
+            itemType = Item.ItemType.HealthPotion,
+            itemAmountt = 5,
+            itemName = "sword",
+
+        });
+
+        inventory.AddItem(new Item {
+            itemType = Item.ItemType.StaminaPotion,
+            itemAmountt = 10,
+            itemName = "sta"
+        });
+
+       
+
+        inventory.printList();
+        Debug.Log("-----------");
+        inventory.searchItem("sta");
+        inventory.RemoveItem("sta");
+
+        RefreshInventory();
+        
+        //CountItems();
+        /* inventory.AddItem(new Item {
+             itemType = Item.ItemType.HealthPotion,
+             amount = 5
+         });*/
+        //inventory = player.GetComponents<PlayerScript>().
+        //playerrScript = player.GetComponent<PlayerScript>();
+        //playerrScript.playerInventory
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+        
+        Debug.Log("amount tesT: " + slot2.transform.GetComponentInChildren<Item>().itemAmountt);
+
+    }
+}
---
+++ SE320/Assets/Scripts/Item.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+
+    public Item (string name, int amount) {
+        itemName = name;
+        itemAmountt = amount;
+
+    }
+
+    public Item() {
+
+    }
+    /*public int itemAmountt { get; set; }
+    public string itemName { get; set; } */
+    public enum ItemType
+    {
+        sword,
+        HealthPotion,
+        ManaPotion,
+        StaminaPotion,
+        StrengthPotion,
+    }
+
+    public enum Sa
+    {
+        selamunaleykum,
+    }
+
+    public Sa sa;
+
+    public ItemType itemType;
+    public int itemAmountt;
+    public string itemName;
+
+    // For UI Item
+    //[SerializeField] private GameObject name;
+    [SerializeField] private GameObject icon;
+    [SerializeField] private GameObject level;
+    [SerializeField] private GameObject quality;
+    // [SerializeField] private GameObject amountt;
+    private GameObject playerObject;
+
+    private PlayerScript player;
+    
+
+
+
+   /* public Item(int ItemType) {
+        if(ItemType == 1) {
+            itemPrefab.GetComponent<Item>().itemType = Item.ItemType.sword;
+            itemPrefab.GetComponent<Item>().itemName = "sword";
+
+        }
+        else {
+            itemPrefab.GetComponent<Item>().itemType = Item.ItemType.HealthPotion;
+            itemPrefab.GetComponent<Item>().itemName = "Healt Potion";
+        }
+        
+    }*/
+
+
+    [SerializeField] private GameObject itemPrefab;
+
+    
+
+
+
+    
+
+    
+
+    public Sprite GetSprite() {
+        switch (itemType) {
+            default:
+            case ItemType.sword: return ItemAssets.Instance.swordSprite;
+            case ItemType.HealthPotion: return ItemAssets.Instance.healthPotionSprite;
+            case ItemType.StaminaPotion: return ItemAssets.Instance.staminaPotionSprite;
+
+
+        }
+
+    }   
+    
+    public void UseItem() { // ItemType itemType
+        if (itemType == ItemType.StaminaPotion) {
+            Debug.Log("stamina potion is used!");
+            if(playerObject.GetComponent<PlayerScript>().currentSTA < playerObject.GetComponent<PlayerScript>().maxSTA) {
+                playerObject.GetComponent<PlayerScript>().currentSTA += 30;
+                if(playerObject.GetComponent<PlayerScript>().currentSTA > playerObject.GetComponent<PlayerScript>().maxSTA) {
+                    playerObject.GetComponent<PlayerScript>().currentSTA = playerObject.GetComponent<PlayerScript>().maxSTA;
+                }
+                itemAmountt--;
+            }
+            
+        }
+        if(itemType == ItemType.HealthPotion) {
+            Debug.Log("health potion is used!");
+            playerObject.GetComponent<PlayerScript>().currentHP += 30;
+        }
+    }
+    
+    
+    // Start is called before the first frame update
+        void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        icon.GetComponent<Image>().sprite = GetSprite();
+    }
+}
